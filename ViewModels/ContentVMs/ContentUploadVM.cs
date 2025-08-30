021a07e3d0691788f8419440f490581c40d43d54
Aruna Lakmal <lakmalarunascc@gmail.com>
2025-08-30 18:11:39 +0530

    CreateContentItemsUpload

---
+++ ViewModels/ContentVMs/ContentUploadVM.cs
@@ -0,0 +1,11 @@
+﻿namespace LMS.ViewModels.ContentVMs
+{
+
+    
+        public class ContentUploadVM
+        {
+        public List<IFormFile> Files { get; set; }
+        public string Name = string.Empty;
+    }
+    
+}
