274961c29abfdac149a949ff193ee71d0ce230de
PasanNevinda <wapasannewinda@gmail.com>
2025-09-14 13:55:11 +0530

    admin dashboard ui and some features

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.Controllers
+{
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public IActionResult AdminDashBoard()
+        {
+            ViewData["Title"] = "Dashboard";
+            return View();
+        }
+    }
+}
